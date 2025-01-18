4b84101f222db6594f8456f1ab195333467f25b9
manikcr24 <manik.cr24@gmail.com>
2025-01-18 20:02:02 +0530

    first commit of siri practice repo

---
+++ frontend/script.js
@@ -0,0 +1,14 @@
+var todos = ['Buy groceries', 'Clean room', 'Do laundry', 'Walk the dog'];
+
+// <li class="todo-item">Buy groceries</li>
+
+
+// <li class="todo-item">Buy groceries</li>
+var liElement = document.createElement('li');
+liElement.className = 'todo-item';
+liElement.innerText = 'Buy groceries';
+
+
+var ulElement = document.getElementById('todoitems');
+ulElement.appendChild(liElement);
+
